3ecabd149a7176667f6cbbb129d4163c88f68d87
Sabaniki <sawada13@mccc.jp>
2019-12-28 23:13:37 +0900

    Amplifyのためのモジュール類を完全削除した

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { MainPageCardBigComponent } from './main-page/main-page-card/main-page-card-big/main-page-card-big.component';
 import { MainPageCardSmallComponent } from './main-page/main-page-card/main-page-card-small/main-page-card-small.component';
 import { AddLectureComponent } from './add-lecture/add-lecture.component';
-import {AmplifyAngularModule, AmplifyService} from 'aws-amplify-angular';
-import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
@@ -18,14 +16,12 @@ import { LoginComponent } from './login/login.component';
     MainPageCardBigComponent,
     MainPageCardSmallComponent,
     AddLectureComponent,
-    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AmplifyAngularModule
   ],
-  providers: [AmplifyService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
