b63f9047d58baf266a816ca48399a5b8509ad232
Sabaniki <sawada13@mccc.jp>
2019-12-29 16:43:12 +0900

    AWSをやめてFirebaseに浮気する準備を進めている（FirebaseConfig生成した）

---
+++ FirebaseConfig.ts
@@ -0,0 +1,10 @@
+export let firebaseConfig = {
+  apiKey: 'AIzaSyCkrOA6mh-bBIxwqRoZVy3JykDI-CPP6xU',
+  authDomain: 'evin-note.firebaseapp.com',
+  databaseURL: 'https://evin-note.firebaseio.com',
+  projectId: 'evin-note',
+  storageBucket: 'evin-note.appspot.com',
+  messagingSenderId: '554577772921',
+  appId: '1:554577772921:web:0cfe309062dcb457ef2fa8',
+  measurementId: 'G-DMZVEHCNX9'
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { MainPageCardBigComponent } from './main-page/main-page-card/main-page-card-big/main-page-card-big.component';
-import { MainPageCardSmallComponent } from './main-page/main-page-card/main-page-card-small/main-page-card-small.component';
-import { AddLectureComponent } from './add-lecture/add-lecture.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MainPageComponent} from './main-page/main-page.component';
+import {MainPageCardBigComponent} from './main-page/main-page-card/main-page-card-big/main-page-card-big.component';
+import {MainPageCardSmallComponent} from './main-page/main-page-card/main-page-card-small/main-page-card-small.component';
+import {AddLectureComponent} from './add-lecture/add-lecture.component';
 
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {firebaseConfig} from '../../FirebaseConfig';
 
 @NgModule({
   declarations: [
@@ -20,8 +24,13 @@ import { AddLectureComponent } from './add-lecture/add-lecture.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
