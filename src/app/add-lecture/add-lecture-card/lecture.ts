e35223d14370c37ea787eb13e165d7ac1abe41f1
Sabaniki <sawada13@mccc.jp>
2020-01-27 03:21:13 +0900

    とりあえずデータベースに登録することにやっっっっっっと成功した。

--- src/app/add-lecture/add-lecture-card/lecture.ts
+++ src/app/add-lecture/add-lecture-card/lecture.ts
@@ -1,14 +1,18 @@
 import {MainPageCard} from '../../main-page/main-page-card/main-page-card';
+import {AuthService} from '../../auth/auth.service';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 export class Lecture {
   constructor(
     public imgName: string,
     public title: string,
+    public teacherName: string,
     public explanatoryText: string,
-    public onClick: (i: number) => void
+    public onClick: (i: number) => void,
+    public auth: AuthService,
+    public afs: AngularFirestore,
   ) {
   }
-  lectureName: string;
-  teacherName: string;
+  lectureCards: Array<Lecture>;
   lastUpdate: string;
 }
