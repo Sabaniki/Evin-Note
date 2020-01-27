fd6cf4bd2f1323086b0bca1197afef543ce26dc8
Sabaniki <sawada13@mccc.jp>
2020-01-28 01:39:08 +0900

    おれとしたことが錬成したクソコード群をステージングするのを忘れていたようだ…笑 ↑○んでくれ

---
+++ src/app/read-note/read-note.component.ts
@@ -0,0 +1,123 @@
+import { Component, OnInit } from '@angular/core';
+import {Lecture} from '../add-lecture/add-lecture-card/lecture';
+import {Router} from '@angular/router';
+import {MonitorScreenSizeService} from '../monitor-screen-size/monitor-screen-size.service';
+import {AuthService} from '../auth/auth.service';
+import {AngularFirestore} from '@angular/fire/firestore';
+import swal from 'sweetalert';
+
+@Component({
+  selector: 'app-read-note',
+  templateUrl: './read-note.component.html',
+  styleUrls: ['./read-note.component.scss']
+})
+export class ReadNoteComponent implements OnInit {
+  lectureCards: Array<Lecture> = [
+    new Lecture(
+      '数学アイコン.svg',
+      '類型数学（多胡）',
+      '多胡',
+      '○曜日✗, △時間目の多胡先生の類型数学の授業です。',
+      this.onClickRedirectNoteButton,
+      this.auth,
+      this.afs
+    ),
+    new Lecture(
+      '物理アイコン.svg',
+      '類型物理（小佐野）',
+      '小佐野',
+      '○曜日✗, △時間の小佐野先生の類型物理の授業です。',
+      this.onClickRedirectNoteButton,
+      this.auth,
+      this.afs
+    ),
+    new Lecture(
+      'はてなマークのアイコン.svg',
+      '？？？？？？？？？？',
+      '？？？？',
+      '？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？',
+      this.onClickRedirectNoteButton,
+      this.auth,
+      this.afs
+    ),
+    new Lecture(
+      'はてなマークのアイコン.svg',
+      '？？？？？？？？？？',
+      '？？？？',
+      '？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？',
+      this.onClickRedirectNoteButton,
+      this.auth,
+      this.afs
+    ),
+    new Lecture(
+      'はてなマークのアイコン.svg',
+      '？？？？？？？？？？',
+      '？？？？',
+      '？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？',
+      this.onClickRedirectNoteButton,
+      this.auth,
+      this.afs
+    ),
+    new Lecture(
+      'はてなマークのアイコン.svg',
+      '？？？？？？？？？？',
+      '？？？？',
+      '？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？',
+      this.onClickRedirectNoteButton,
+      this.auth,
+      this.afs
+    )
+  ];
+
+  constructor(private router: Router,
+              public monitorScreenSizeService: MonitorScreenSizeService,
+              private auth: AuthService,
+              private afs: AngularFirestore
+  ) {
+  }
+
+  ngOnInit() {
+    this.auth.user$.subscribe(user => {
+      // for-ofでやると参照的なアレで変更ができない気がしたから抑制
+      // tslint:disable-next-line:prefer-for-of
+      for (let i = 0; i < this.lectureCards.length; i++) {
+        this.lectureCards[i].lectureCards = this.lectureCards;
+        this.afs.doc(`users/${user.uid}`)
+          .collection('lectures')
+          .doc(this.lectureCards[i].title).get()
+          .subscribe(
+            doc => this.lectureCards[i].isRegistered = (this.lectureCards[i].title === (doc.data() as Lecture).title)
+          );
+      }
+    });
+  }
+
+  onClickRedirectNoteButton(i: number) {
+    this.auth.user$.subscribe(user => {
+      console.log(user);
+      if (!!user) {
+        this.afs.doc(`users/${user.uid}`).collection('lectures')
+          .doc(this.lectureCards[i].title)
+          .set(Object.assign({}, {
+            title: this.lectureCards[i].title,
+            teacherName: this.lectureCards[i].teacherName,
+            explanatoryText: this.lectureCards[i].explanatoryText
+          }))
+          .then(() => {
+              swal({
+                text: '講義を登録しました！',
+                icon: 'success',
+              });
+              this.lectureCards[i].isRegistered = true;
+            },
+            reason => {
+              swal({
+                text: 'エラーが発生しました！' + reason.toString(),
+                icon: 'error',
+              });
+            }
+          );
+      }
+    });
+  }
+}
